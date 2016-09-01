24763c41e30fd6020749e3e1c7116f175e0058b9
Jack Christensen <jack@jackchristensen.com>
2016-09-01 09:17:33 -0500

    Update readline for cancellable stdin

--- vendor/github.com/chzyer/readline/terminal.go
+++ vendor/github.com/chzyer/readline/terminal.go
@@ -3,6 +3,7 @@ package readline
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -17,6 +18,8 @@ type Terminal struct {
 	wg        sync.WaitGroup
 	isReading int32
 	sleeping  int32
+
+	sizeChan chan string
 }
 
 func NewTerminal(cfg *Config) (*Terminal, error) {
@@ -28,6 +31,7 @@ func NewTerminal(cfg *Config) (*Terminal, error) {
 		kickChan: make(chan struct{}, 1),
 		outchan:  make(chan rune),
 		stopChan: make(chan struct{}, 1),
+		sizeChan: make(chan string, 1),
 	}
 
 	go t.ioloop()
@@ -58,6 +62,18 @@ func (t *Terminal) ExitRawMode() (err error) {
 
 func (t *Terminal) Write(b []byte) (int, error) {
 	return t.cfg.Stdout.Write(b)
+}
+
+type termSize struct {
+	left int
+	top  int
+}
+
+func (t *Terminal) GetOffset(f func(offset string)) {
+	go func() {
+		f(<-t.sizeChan)
+	}()
+	t.Write([]byte("\033[6n"))
 }
 
 func (t *Terminal) Print(s string) {
@@ -94,7 +110,11 @@ func (t *Terminal) KickRead() {
 
 func (t *Terminal) ioloop() {
 	t.wg.Add(1)
-	defer t.wg.Done()
+	defer func() {
+		t.wg.Done()
+		close(t.outchan)
+	}()
+
 	var (
 		isEscape       bool
 		isEscapeEx     bool
@@ -132,7 +152,24 @@ func (t *Terminal) ioloop() {
 			r = escapeKey(r, buf)
 		} else if isEscapeEx {
 			isEscapeEx = false
-			r = escapeExKey(r, buf)
+			if key := readEscKey(r, buf); key != nil {
+				r = escapeExKey(key)
+				// offset
+				if key.typ == 'R' {
+					if _, _, ok := key.Get2(); ok {
+						select {
+						case t.sizeChan <- key.attr:
+						default:
+						}
+					}
+					expectNextChar = true
+					continue
+				}
+			}
+			if r == 0 {
+				expectNextChar = true
+				continue
+			}
 		}
 
 		expectNextChar = true
@@ -150,7 +187,7 @@ func (t *Terminal) ioloop() {
 			t.outchan <- r
 		}
 	}
-	close(t.outchan)
+
 }
 
 func (t *Terminal) Bell() {
@@ -161,8 +198,11 @@ func (t *Terminal) Close() error {
 	if atomic.SwapInt32(&t.closed, 1) != 0 {
 		return nil
 	}
-	t.stopChan <- struct{}{}
-	// t.wg.Wait()
+	if closer, ok := t.cfg.Stdin.(io.Closer); ok {
+		closer.Close()
+	}
+	close(t.stopChan)
+	t.wg.Wait()
 	return t.ExitRawMode()
 }
 
